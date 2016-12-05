0c50b298d485d32c5f47eb2c5b501f6bb9582c23
laurakathleen <laurakwalley@gmail.com>
2016-12-04 18:10:59 -0800

    adds fadeIn

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,4 +1,28 @@
 $(function(){
 	$(document).foundation();
 	console.log('sanity check');
+
+	fadeInUp('#about-me-blurb', 370);
+	fadeInUp('.all-steps', 855);
+	fadeInUp('.work-flow-text', 855);
+	fadeInUp("#a-few-short-miles", 1280);
+	fadeInUp("#few-short-miles-preview", 1280);
+	fadeInUp("#within-reach", 1860);
+	fadeInUp("#within-reach-preview", 1860);
+	fadeInUp("#the-best-hour", 2430);
+	fadeInUp("#best-hour-preview", 2430);
+	fadeInUp(".contact-text", 3135);
+	fadeInUp(".icon-container", 3135);
+	fadeInUp(".icon-container", 3135);
+	fadeInUp('#copyright', 3135);
+
+	function fadeInUp(element, height){
+    $(window).scroll(function() { 
+        if ($(this).scrollTop() > height) { 
+            $(element).css('visibility','visible');   
+            $(element).addClass('animated fadeInUp');  
+        } 
+    });
+};
+
 });
